606929039cfd3ad5cde070b70a0c66775c9eceb9
iabdelazeez <71781112+iabdelazeez@users.noreply.github.com>
2020-09-27 21:13:08 +0300

    0.1.0
    2 day
    i have FUN(t****)

---
+++ 3D 0.1.0/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+ public float MouseSensitivity = 350f;
+ 
+ public Transform PlayerBody;   
+
+ float xRotation = 0f;
+    
+    void start ()
+    {
+      Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f , 90f ); 
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);       
+        PlayerBody.Rotate(Vector3.up * MouseX);
+    }
+    
+}
---
+++ 3D 0.1.0/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public CharacterController Controller;
+
+  public float Speed = 12f;
+  public float Gravity = -9.81f;
+  public float jumpheight = 3f;
+
+  public Transform GroundChack;
+  public float GroundDistance = 0.4f;
+  public LayerMask GroundMask;
+  bool isgrounded;
+  Vector3 velocity;
+
+ 
+     ////////////////////////////////////////// 
+    void Update()
+    {
+      movement();
+      JumpAndGravity();
+      
+    }
+
+    //////////////////////////////////////////
+    void movement()
+    {
+    //input geting
+      float x = Input.GetAxis("Horizontal");
+      float z = Input.GetAxis("Vertical");
+    //we have input we use input for math to move YOU
+      Vector3 move = transform.right * x + transform.forward * z;
+    //now lt`s move  
+      Controller.Move(move * Speed * Time.deltaTime);
+    }
+
+    /////////////////////////////////////////////////
+    void JumpAndGravity()
+    {
+     //isground Check
+     isgrounded = Physics.CheckSphere(GroundChack.position, GroundDistance, GroundMask);
+     //if yes and it is did`not zero it 
+     if(isgrounded && velocity.y <0) { velocity.y = -2f; }//good you can jump now
+     
+      //if you press jump Key"space" + you are on ground = You jump
+      if(Input.GetButtonDown("Jump") && isgrounded)
+      {//some math
+        velocity.y = Mathf.Sqrt(jumpheight * -2f * Gravity);
+      }
+       ///////////Gravity//////////////
+      velocity.y += Gravity * Time.deltaTime;
+      //yes Gravity are moveing YOU
+      Controller.Move(velocity * Time.deltaTime); 
+    }
+
+    
+
+
+
+}
